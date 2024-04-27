79dc1e203a26eaaa4ddf0b74941b054ee2e26fd8
Diogo Caxiado <caxiadodev@gmail.com>
2024-04-27 18:21:02 -0300

    feat: Adicionando o framer-motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Card } from "@/components/Card/card";
+import { motion } from "framer-motion";
 
 import styles from "./page.module.scss";
 import { useDataContext } from "@/context/product";
@@ -13,15 +14,21 @@ export default function Home() {
       {
         products.map((product) => {
           return (
-            <Card 
+            <motion.div 
               key={product.id}
-              id={product.id}
-              name={product.name} 
-              description={product.description} 
-              src={product.photo} 
-              price={product.price}
-              amount={1}
-            />
+              initial={{ x: -1000 }}
+              animate={{ x: 0 }}
+              transition={{type: 'spring', damping: 5, mass: 0.5, stiffness: 30 }}
+            >
+              <Card 
+                id={product.id}
+                name={product.name} 
+                description={product.description} 
+                src={product.photo} 
+                price={product.price}
+                amount={1}
+              />
+            </motion.div>
           )
         })
       }
--- src/components/Checkout/checkout.tsx
+++ src/components/Checkout/checkout.tsx
@@ -1,9 +1,12 @@
 'use client'
 
 import { ICheckout } from '@/interface';
-import styles from './checkout.module.scss';
+import { motion } from "framer-motion";
+
 import { useDataContext } from '@/context/product';
 import { Item } from '../Item/item';
+
+import styles from './checkout.module.scss';
 
 export function Checkout({onClose}: ICheckout) {
    const {itemsInCart} = useDataContext();
@@ -31,15 +34,30 @@ export function Checkout({onClose}: ICheckout) {
 
          <div className={styles.items}>
             {itemsInCart.map((item) => {
-               return <Item key={item.id} product={item} />
+               return (
+                  <motion.div 
+                     key={item.id}
+                     initial={{ x: 400 }}
+                     animate={{ x: 0 }}
+                     transition={{type: 'spring', damping: 6, mass: 0.5, stiffness: 30 }}
+                     style={{display:'flex', justifyContent: 'center', width: '100%'}}
+                  >
+                     <Item product={item} />
+                  </motion.div>
+               );
             })}
          </div>
 
          <div className={styles.bottom}>
-            <div className={styles.info}>
+            <motion.div 
+               className={styles.info}
+               initial={{ x: 400 }}
+               animate={{ x: 0 }}
+               transition={{type: 'spring', damping: 6, mass: 0.5, stiffness: 30 }}         
+            >
                <strong className={styles.text}>Total:</strong>
                <strong className={styles.price}>{handleTotalValue()}</strong>
-            </div>
+            </motion.div>
 
             <footer className={styles.button}>
                <span>Finalizar Compra</span>
